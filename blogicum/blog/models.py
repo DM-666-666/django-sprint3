6f6759fb256878d2b0cc5da695b4cd284040589c
Dmitry Meshcheryakov <Dima23232424@yandex.ru>
2026-03-04 01:07:39 +0300

    Changed verboses in models.py

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -3,20 +3,20 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст поста')
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
-        auto_now_add=True, verbose_name='Дата публикации')
+        auto_now_add=True, verbose_name='Дата и время публикации')
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, verbose_name='Дата Публикации')
+        User, on_delete=models.CASCADE, verbose_name='Автор Публикации')
     location = models.ForeignKey(
-        'Location', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Локация')
+        'Location', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Местоположение')
     category = models.ForeignKey(
         'Category', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Категория')
     is_published = models.BooleanField(
         default=True, verbose_name='Опубликовано')
     created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name='Создано')
+        auto_now_add=True, verbose_name='Добавлено')
 
     def __str__(self):
         return self.title
@@ -27,13 +27,13 @@ class Post(models.Model):
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=256, verbose_name='Название')
-    description = models.TextField(verbose_name='Текст категории')
-    slug = models.SlugField(unique=True, verbose_name='Слаг')
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(unique=True, verbose_name='Идентификатор')
     is_published = models.BooleanField(
         default=True, verbose_name='Опубликовано')
     created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name='Создано')
+        auto_now_add=True, verbose_name='Добавлено')
 
     def __str__(self):
         return self.title
@@ -44,11 +44,11 @@ class Category(models.Model):
 
 
 class Location(models.Model):
-    name = models.CharField(max_length=256, verbose_name='Название')
+    name = models.CharField(max_length=256, verbose_name='Название места')
     is_published = models.BooleanField(
         default=True, verbose_name='Опубликовано')
     created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name='Создано')
+        auto_now_add=True, verbose_name='Добавлено')
 
     def __str__(self):
         return self.name
