e0d7c078b2f1919f6614de60fd3b88ad3b42142b
Dmitry Meshcheryakov <Dima23232424@yandex.ru>
2026-03-04 00:58:04 +0300

    created blog/models.py, created Post, Category and Location class

---
+++ blogicum/blog/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст поста')
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата публикации')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Дата Публикации')
+    location = models.ForeignKey(
+        'Location', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Локация')
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Категория')
+    is_published = models.BooleanField(
+        default=True, verbose_name='Опубликовано')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Создано')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'Публикации'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    description = models.TextField(verbose_name='Текст категории')
+    slug = models.SlugField(unique=True, verbose_name='Слаг')
+    is_published = models.BooleanField(
+        default=True, verbose_name='Опубликовано')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Создано')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    is_published = models.BooleanField(
+        default=True, verbose_name='Опубликовано')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Создано')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Местоположение'
+        verbose_name_plural = 'Метоположения'
