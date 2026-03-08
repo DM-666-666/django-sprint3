33cc037451e2856ba23fe40025fac4584719ba39
Dmitry Meshcheryakov <Dima23232424@yandex.ru>
2026-03-09 00:27:14 +0300

    custom admin page

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from .models import Post, Category, Location
 
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Location)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "text", "category", "created_at")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "created_at")
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at")
